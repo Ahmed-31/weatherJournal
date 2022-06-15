8467bbf60fc85921cbc92027c6fd36671b62de03
Ahmed-31 <77246783+Ahmed-31@users.noreply.github.com>
2022-06-16 01:09:46 +0200

    added fun to get the api weather data with api key

--- website/app.js
+++ website/app.js
@@ -62,7 +62,53 @@ getDataFromLocal('http://127.0.0.1:8080/all').then((data) => {
 document.querySelector('#generate').addEventListener('click', onClick);
 
 function onClick() {
-    console.log('click generate');
+    // check if the zip text empty or not I as a pro programmer did this for a good reason
+    //which is to separate errors invalid zip and not even enter zip :)
+    if (document.querySelector('#zip').value) {
+        const zipValue = document.querySelector('#zip').value;
+        // call the function to get web api data
+        weatherData(zipValue).then((weatherdata) => {
+            //log the respons data 
+            console.log('weatherdata', weatherdata);
+        });
+    } else {
+        alert('Can\'t get data without zip code!!\nPlease enter zip');
+    }
+
+
+}
+
+// Personal API Key for OpenWeatherMap API
+//the api key is from api.openweathermap.org site and it's for one user only
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const APIKey = ',&appid=5aca0bbb23343940f01aad543cba2b1c&units=metric';
+
+// Function to get the weather data
+async function weatherData(zipValue) {
+    //get the data from the api 
+    const response = await fetch(baseURL + zipValue + APIKey); // GET by default
+    try {
+        const responseData = await response.json();
+        //if any error in the respond from api then print it and throw error
+        if (responseData.cod !== 200) {
+            throw new Error(responseData.message);
+        } else {
+            //get the data from the promise response then return it as an object
+            const temprature = responseData.main.temp;
+            const cityName = responseData.name;
+            const weatherData = {
+                cityName,
+                temprature
+            }
+            return weatherData;
+        }
+    } catch (error) {
+        console.log('error', error); //print error for me 'ahmed adel' in the console
+        alert(error); //prompt a message to the user to inform him he is idiot not entering zip code
+        return reject(error);
+    }
+
+
 }
 
 // const x = async() => {
